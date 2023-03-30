c258db19e301088beecda0b951798ad49fc6429e
Eli Friedlander <eli.friedlander@gmail.com>
2023-03-30 14:39:54 -0400

    Needed some help but got it done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const birthday = ["Charlie", "Samip", "Ali"];
+
+function writeCards(birthday) {
+  const emptyArray =[]
+for (let i = 0; i < birthday.length; i++) {
+emptyArray.push(`Thank you, ${birthday[i]}, for the wonderful surprise gift!`)
+}
+return emptyArray}
+
+function countDown(n) {
+
+let countDown = n
+while (countDown >= 0){
+    console.log(countDown --)
+}}
